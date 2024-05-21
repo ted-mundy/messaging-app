5e379535f91e1c037f2f30048fb41f6e1a2844a2
Ted Mundy <ted@mundy.io>
2024-05-21 17:35:28 +0100

    feat(message-service): add diesel to project

--- services/message-service/src/handlers/conversations.rs
+++ services/message-service/src/handlers/conversations.rs
@@ -1,26 +1,23 @@
 use actix_web::{web, HttpRequest, HttpResponse};
 use serde::Deserialize;
 
+// use crate::models::conversation;
+
 #[derive(Deserialize)]
-pub struct Conversation {
-  id: usize,
-  // user_id: usize,
-  // messages: Vec<String>,
+pub struct ConversationFilter {
+  user_id: Option<usize>,
 }
 
-pub async fn get_conversations(_req: HttpRequest, info: web::Query<Conversation>) -> HttpResponse {
-  // let user_id = _req.match_info().get("user_id").unwrap_or_else(|| "");
-  // println!("User ID: {}", user_id);
-  // if user_id == "" {
-  //   return HttpResponse::BadRequest().body("User ID is required");
-  // }
-  // if user_id.parse::<usize>().is_err() {
-  //   return HttpResponse::BadRequest().body("User ID must be a number");
-  // }
+pub async fn get_conversations(_req: HttpRequest, info: web::Query<ConversationFilter>) -> HttpResponse {
+  let user_id: Option<usize> = info.user_id;
 
-  // HttpResponse::Ok().body(format!("GET conversations for user {}", user_id))
+  // let r = user_id.and_then(|id| {
+  //   if id == 1 {
+  //     Some("User 1's conversations")
+  //   } else {
+  //     None
+  //   }
+  // }).unwrap_or("No conversations found");
 
-  // HttpResponse::Ok().json(json.0)
-
-  HttpResponse::Ok().json(info.id)
+  HttpResponse::Ok().body("hello")
 }
--- services/message-service/src/main.rs
+++ services/message-service/src/main.rs
@@ -1,10 +1,15 @@
 use actix_web::{HttpServer, App};
 
+use dotenv::dotenv;
+
 mod routes;
 mod handlers;
+mod models;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+  dotenv().ok();
+
   HttpServer::new(move || {
     App::new()
       .configure(routes::init_routes)
