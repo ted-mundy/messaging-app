865961c0cefaa1f835cd9239c28b015450e9ac11
Ted Mundy <ted@mundy.io>
2024-05-22 14:03:43 +0100

    feat(messaging-service): change get_conversations action to struct based

--- services/message-service/src/db/conversation_actions.rs
+++ services/message-service/src/db/conversation_actions.rs
@@ -1,13 +1,25 @@
 use diesel::{self, query_dsl::methods::LimitDsl, r2d2::{ConnectionManager, PooledConnection}, QueryResult, RunQueryDsl};
+use serde::Deserialize;
+use uuid::Uuid;
 
 use crate::models::conversation::Conversation;
 
+#[derive(Deserialize)]
+pub struct ConversationFilter {
+  pub user_uuid: Option<Uuid>,
+}
+
+pub struct GetConversationConfig {
+  pub db_pool: PooledConnection<ConnectionManager<diesel::PgConnection>>,
+  pub filter: ConversationFilter
+}
+
 pub async fn get_conversations(
-  mut db_conn: PooledConnection<ConnectionManager<diesel::PgConnection>>,
+  mut config: GetConversationConfig,
 ) -> QueryResult<Conversation> {
   use crate::db::schema::conversations::dsl::*;
 
   conversations
     .limit(10)
-    .get_result::<Conversation>(&mut db_conn)
+    .get_result::<Conversation>(&mut config.db_pool)
 }
--- services/message-service/src/handlers/conversations.rs
+++ services/message-service/src/handlers/conversations.rs
@@ -1,18 +1,15 @@
 use actix_web::{web, HttpResponse};
 use diesel::r2d2::{self, ConnectionManager};
-use serde::Deserialize;
 
-use crate::db::conversation_actions;
-
-#[derive(Deserialize)]
-pub struct ConversationFilter {
-  user_id: Option<usize>,
-}
+use crate::db::conversation_actions::{self, ConversationFilter};
 
 pub async fn get_conversations(info: web::Query<ConversationFilter>, db_pool: web::Data<r2d2::Pool<ConnectionManager<diesel::PgConnection>>>) -> HttpResponse {
   let db_conn = db_pool.get().unwrap();
 
-  let conversations = conversation_actions::get_conversations(db_conn).await;
+  let conversations = conversation_actions::get_conversations(conversation_actions::GetConversationConfig {
+    db_pool: db_conn,
+    filter: info.into_inner(),
+  }).await;
 
   match conversations {
     Ok(conversations) => HttpResponse::Ok().json(conversations),
